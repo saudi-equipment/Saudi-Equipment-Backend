6890ab43142509daf20155cd2521b30df5900124
Muhammad-athar105 <athar9157@gmail.com>
2025-04-09 02:10:54 +0500

    fixed the issues in ad, report, user

--- src/user/dtos/update.user.dto.ts
+++ src/user/dtos/update.user.dto.ts
@@ -1,6 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
-import { IsBoolean, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  IsBoolean,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class UserUpdateDto {
   @ApiProperty({ description: 'The English name of the user' })
@@ -56,4 +62,42 @@ export class UserUpdateDto {
   @IsBoolean()
   @Transform(({ value }) => value === 'true' || value === true)
   isPremiumUser: boolean;
+
+  @IsOptional()
+  @ApiProperty({ description: 'Start Date of the sub scription' })
+  startDate: Date;
+
+  @IsOptional()
+  @ApiProperty({ description: 'End Date of the sub scription' })
+  endDate: Date;
+
+  @ApiProperty({ description: 'The plan of the prmium user' })
+  @IsString()
+  @IsOptional()
+  readonly plan: string;
+
+  @ApiProperty({ description: 'The duration of the plan' })
+  @IsString()
+  @IsOptional()
+  readonly duration: string;
+
+  @ApiProperty({ description: 'The price of the plan' })
+  @IsString()
+  @IsOptional()
+  readonly price: string;
+
+  @ApiProperty({ description: 'The payment type of the plan' })
+  @IsString()
+  @IsOptional()
+  readonly paymentType: string;
+
+  @ApiProperty({ description: 'The paymentCompany of the plan' })
+  @IsString()
+  @IsOptional()
+  readonly paymentCompany: string;
+
+  @ApiProperty({ description: 'The subscriptionId of the prmium user' })
+  @IsString()
+  @IsOptional()
+  readonly subscriptionId: string;
 }
